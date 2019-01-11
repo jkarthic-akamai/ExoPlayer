bde20e6791f1692b3407f64532f11020a7ee76f3
Karthick J <kjeyapal@akamai.com>
2019-01-11 12:04:59 +0530

    Simple Activity with configurable input URL for Player

---
+++ demos/main/src/main/java/com/google/android/exoplayer2/demo/URLInputActivity.java
@@ -0,0 +1,43 @@
+package com.google.android.exoplayer2.demo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+/**
+ * Created by kjeyapal on 10/10/17.
+ */
+
+public class URLInputActivity extends Activity {
+  Button mPlayButton;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.hls_dash_playout);
+
+    mPlayButton = (Button) findViewById(R.id.playButton);
+    mPlayButton.setOnClickListener(new OnStartButtonClick(this));
+  }
+  private class OnStartButtonClick implements View.OnClickListener {
+    Context mContext;
+    OnStartButtonClick(Context context) {
+      mContext = context;
+    }
+    @Override
+    public void onClick(View v) {
+      EditText urlText = (EditText) findViewById(R.id.urlInput);
+      String playout_url = urlText.getText().toString();
+      Intent intent = new Intent(mContext, PlayerActivity.class);
+      intent.setData(Uri.parse(playout_url));
+      intent.setAction(PlayerActivity.ACTION_VIEW);
+      startActivity(intent);
+    }
+  }
+}
